da0d085d06c668044656fba8d817d6c6b85527dc
soerinev <laura.soerine@icloud.com>
2024-09-26 23:49:48 +0200

    added setup doc

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# how to setup automatically when I open Ucloud!/usr/bin/env
+
+git config --global user.email "laura.soerine@icloud.com"
+git config --global user.name "soerinev"
+
+# before adding, committing and pushing to github, press "bash setup.sh", that's the same as writing the above?? not necessary because of !/usr/bin/env?? 
